444486ad5100b8356fbd6c4b480ad94ec6bac286
ParthGondaliya <parthpatel848884@gmail.com>
2025-02-07 14:15:34 +0530

    add templates

--- cbv_crud_operation/cbv_crud_app/urls.py
+++ cbv_crud_operation/cbv_crud_app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from cbv_crud_app import views
+
 urlpatterns = [
-    path('',views.hello,name='hello'),
+    path('',views.HomeTemplate.as_view(),name='home'),
+    path('list_data/',views.TeamDetailListView.as_view(),name='list_data'),
+    path('create_data/',views.TeamDetailCreateView.as_view(),name='create_data'),
+    path('detail_data/<int:pk>/',views.TeamDetailDetailView.as_view(),name='detail_data'),
+    path('update_data/<int:pk>/',views.TeamDetailUpdateView.as_view(),name='update_data'),
+    path('delete_data/<int:pk>/',views.TeamDetailDeleteView.as_view(),name='delete_data'),
 ]
--- cbv_crud_operation/cbv_crud_app/views.py
+++ cbv_crud_operation/cbv_crud_app/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
-from .forms import TeamDetail
-from django.http import HttpResponse
+from .forms import TeamDetailForm
+from .models import TeamDetail
+from django.views.generic import UpdateView, RedirectView, DeleteView, ListView, DetailView,TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-def hello(request):
-    return HttpResponse('hello')
+
+class HomeTemplate(TemplateView):
+    template_name = 'team_detail/home.html'
+
+
+class TeamDetailListView(ListView):
+    model = TeamDetail
+    template_name = 'team_detail/list_data.html'
+    context_object_name = 'team_detail_list_data'
+    
+
+class TeamDetailDetailView(DetailView):
+    model = TeamDetail
+    template_name = 'team_detail/detail_data.html'
+    context_object_name = 'team_detail_detail_data'
+
+
+class TeamDetailCreateView(CreateView):
+    model = TeamDetail
+    form_class = TeamDetailForm
+    template_name = 'team_detail/create_data.html'
+
+    success_url = reverse_lazy('list_data')
+
+
+class TeamDetailUpdateView(UpdateView):
+    model = TeamDetail
+    form_class = TeamDetailForm
+    template_name = 'team_detail/create_data.html'
+    success_url = reverse_lazy('list_data')
+
+
+class TeamDetailDeleteView(DeleteView):
+    model = TeamDetail
+    template_name = 'team_detail/delete_data.html'
+    success_url = reverse_lazy('list_data')
